6dbbf003204623729c0b6c0542400538be406928
younesthestar <younes.najjar.96@gmail.com>
2018-10-02 03:18:10 +0100

    Separated login from swipe Activity + Code enhancements

--- app/src/main/java/com/example/younes/hotornot/ui/Activities/StartingActivity.java
+++ app/src/main/java/com/example/younes/hotornot/ui/Activities/StartingActivity.java
@@ -1,11 +1,7 @@
 package com.example.younes.hotornot.ui.Activities;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.example.younes.hotornot.R;
-import com.example.younes.hotornot.utils.SaveSharedPreference;
 
 public class StartingActivity extends AppCompatActivity {
 
@@ -13,12 +9,5 @@ public class StartingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
-        if(SaveSharedPreference.getLoggedStatus(getApplicationContext())){
-            Intent intent = new Intent(this, SwiperScreenActivity.class);
-            startActivity(intent);
-        }
-        else{
-
-        }
     }
 }
---
+++ app/src/main/java/com/example/younes/hotornot/utils/FaceBookLoginStatus.java
@@ -0,0 +1,8 @@
+package com.example.younes.hotornot.utils;
+
+/**
+ * Created by younes on 10/2/2018.
+ */
+
+public class FaceBookLoginState {
+}
